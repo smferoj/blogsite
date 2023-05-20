5e9ba689741ba255f690c1aa49c9780c4ea94704
SM Feroj <sm.feroj@gmail.com>
2023-05-20 13:48:33 +0600

    login ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      
+     <h1> Login</h1>
+     <form action="">
+      <input type="text" placeholder='username' />
+      <input type="password" placeholder='password' />
+      <button> Log in</button>
+      <p> This is an error!</p>
+      <span> Don't have an account? Got to Register</span>
+     </form>
+
+    </div>
   )
 }
 
